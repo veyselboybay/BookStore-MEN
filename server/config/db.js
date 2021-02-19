a16407ad172b8e12ad061918e68d559e4e137321
veyselboybay <veysel.boybay@outlook.com>
2021-02-19 12:56:45 -0500

    updated URI DB to work with mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI":"mongodb://localhost/book_store"
+    //"URI":"mongodb://localhost/book_store"
+    "URI":"mongodb+srv://veysel:E9QXMwypOHMKFBsO@mongodbserver.7x96x.mongodb.net/bookStore?retryWrites=true&w=majority"
 }
